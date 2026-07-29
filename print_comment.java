3fac71b397b92409712ab73d9ea201803348afff
Sameer Ranjan <sameerranjan090@gmail.com>
2026-07-29 20:59:14 +0530

    Understanding print and comment statements in java

---
+++ print_comment.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args){
+        // This is a java code
+
+        /*
+        This
+         is
+         a
+         multi-line
+         comment
+         */
+
+        System.out.print("I like pizza!\n");
+        System.out.println("It's really good!");
+        System.out.print("8uy me one");
+    }
+}
