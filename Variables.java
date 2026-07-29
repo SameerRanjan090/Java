d714563819fb06e5aef6ad0b90db69d6de814476
Sameer Ranjan <sameerranjan090@gmail.com>
2026-07-29 21:32:36 +0530

    Create Main class with variable examples
    Added a Main class with variable declarations and initializations for different data types.

--- Variables.java
+++ Variables.java
@@ -1,1 +1,34 @@
+public class Main {
 
+    public static void main(String[] args){
+        // 2 steps to creating a variable
+        //-------------------------------
+        // 1. declaration
+        // 2. assignment
+
+        // Primitive data type- simple value stored in stack
+        int age = 20;
+        int year = 2026;
+        int quantity = 1;
+
+        double price = 20.21;
+        double cgpa = 8.89;
+        double temperature = -12.5;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        boolean isStudent = true;
+        boolean forSale =  false;
+        boolean isOnline = true;
+
+        // Reference = memory address (stack) that points to (heap)
+        String name = "Sameer";
+        String food = "Pizza";
+        String email = "fake123@gmail.com";
+        String car = "Ceverlot Traverse";
+        String color = "Grey";
+        // arrays and objects are also reference.
+    }
+}
